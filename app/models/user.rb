8dcc421f681704d991c7f32e526dc8494dc6b771
Uthaeus <romanlavery@gmail.com>
2023-05-19 17:02:30 -0600

    generated scaffold for comments, added authentication and json responsiveness to comment controller

--- app/models/user.rb
+++ app/models/user.rb
@@ -10,5 +10,6 @@ class User < ApplicationRecord
     super 
   end
 
-  has_many :posts 
+  has_many :posts
+  has_many :comments, dependent: :destroy
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
+  resources :comments
   resources :posts
   get 'home', to: 'pages#home'
   get 'user_current', to: 'pages#user_current'
